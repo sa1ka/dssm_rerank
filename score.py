bdc575a7f97d139fd40d8743870a67346686eed6
Sa1ka <saika.ichm@gmail.com>
2017-05-11 14:50:49 +0800

    modify score.py

--- score.py
+++ score.py
@@ -19,48 +19,58 @@ def evaluate(model, data_iter):
 
 if __name__ == '__main__':
 
-    # test model correct rate on valid set
-
-    dictionary = Dictionary('./full_dataset/train.vocab')
-    valid_iter = DataIter(
-        corpus_path =  './full_dataset/valid.txt',
-        batch_size = 100,
-        seq_len = 25,
-        dictionary = dictionary,
-        cuda = True
-    )
-    model_path = './params/BOTH/model.pt'
+    model_path = './params/CNN/model.pt'
     with open(model_path, 'rb') as f:
         model = torch.load(f)
-    print 'cor_rate on valid: {:5.4f}'.format(evaluate(model, valid_iter))
+    dictionary = Dictionary('./full_dataset/train.vocab')
+    print model_path
+
+    if False:
+
+        # test model correct rate on valid set
+        valid_iter = DataIter(
+            corpus_path =  './full_dataset/valid.txt',
+            batch_size = 100,
+            seq_len = 25,
+            dictionary = dictionary,
+            cuda = True
+        )
+        print 'cor_rate on valid: {:5.4f}'.format(evaluate(model, valid_iter))
 
     # test model spearman correlation with human score
 
-    data_path = './score/0504score/'
-    data_iter = DataIter(
-        corpus_path = data_path + 'pc_pairs.txt',
-        batch_size = 1,
-        seq_len = 25,
-        dictionary = dictionary,
-        cuda = True
-    )
-    with open(data_path + 'score.txt') as f:
-        score = map(float, f.read().splitlines())
+    data_pathes = ['./score/0504score/', './score/0420score/', './score/0417score/', './score/0413score/']
+    for data_path in data_pathes:
 
-    model.eval()
-    predict = []
-    for i, d in enumerate(data_iter):
-        post, cmnt, _ = model(d)
-        predict.append(torch.norm(post - cmnt, 2, 1).squeeze(0).data.cpu().numpy()[0])
+        print data_path
+        data_iter = DataIter(
+            corpus_path = data_path + 'pc_pairs.txt',
+            batch_size = 1,
+            seq_len = 25,
+            dictionary = dictionary,
+            cuda = True
+        )
+        with open(data_path + 'score.txt') as f:
+            score = map(float, f.read().splitlines())
 
-    stat = defaultdict(lambda: [])
-    for s, p in zip(score, predict):
-        stat[s].append(p)
+        model.eval()
+        predict = []
+        for i, d in enumerate(data_iter):
+            post, cmnt, _ = model(d)
+            predict.append(torch.norm(post - cmnt, 2, 1).squeeze(0).data.cpu().numpy()[0])
 
-    s1 = []
-    s2 = []
-    for k, v in sorted(stat.iteritems()):
-        s1.append(k)
-        s2.append(sum(v) / len(v))
+        with open(data_path + 'predict.txt', 'w') as f:
+            for p in predict:
+                f.write(str(p) + '\n')
 
-    print spearmanr(s1, s2)
+        stat = defaultdict(lambda: [])
+        for s, p in zip(score, predict):
+            stat[s].append(p)
+
+        s1 = []
+        s2 = []
+        for k, v in sorted(stat.iteritems()):
+            s1.append(k)
+            s2.append(sum(v) / len(v))
+
+        print spearmanr(s1, s2)
